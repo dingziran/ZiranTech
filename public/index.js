d727e20adedba37878a3b16006535878dc92aa50
dingziran <dingziranrr@163.com>
2015-03-23 17:35:27 +0800

    add csv reader

---
+++ public/index.js
@@ -0,0 +1,34 @@
+/**
+ * Created by dingziran on 2015/3/23.
+ */
+var myApp=angular.module("test",['ui.router']);
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/index");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('index', {
+            url: "/index",
+            templateUrl: "index.html"
+        })
+        .state('state1.list', {
+            url: "/list",
+            templateUrl: "partials/state1.list.html",
+            controller: function($scope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+        .state('state2', {
+            url: "/state2",
+            templateUrl: "partials/state2.html"
+        })
+        .state('state2.list', {
+            url: "/list",
+            templateUrl: "partials/state2.list.html",
+            controller: function($scope) {
+                $scope.things = ["A", "Set", "Of", "Things"];
+            }
+        });
+});
